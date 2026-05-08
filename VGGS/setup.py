9130a04d20471fec761e112801da7ed17a5042bc
Bram <51411007+BramKlingeler@users.noreply.github.com>
2026-05-08 10:47:57 +0200

    Update setup.py

--- VGGS/setup.py
+++ VGGS/setup.py
@@ -5,6 +5,6 @@ from setuptools import find_packages, setup
 setup(
     name="ggs",
     version="0.0.2",
-    description="This project implements Gibbs sampling with Graph-based Smoothing (GGS) on GFP and AAV data ",
+    description="This project implements Varying length Gibbs sampling with Graph-based Smoothing (GGS) on SARS-CoV-2 data ",
     packages=find_packages(),
 )
